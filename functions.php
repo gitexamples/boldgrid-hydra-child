39cb706316720358d54c8b870cb430906e406988
Harry Jackson <harryjackson1221@gmail.com>
2018-01-09 12:13:30 -0500

    Update functions.php
    Removed space after boldgrid_enqueue_styles is called, was causing a PHP error in some cases

--- functions.php
+++ functions.php
@@ -6,5 +6,5 @@ function boldgrid_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), 
 wp_get_theme()->get('Version') );
 }
-add_action( 'wp_enqueue_scripts', 'boldgrid_enqueue_styles ' );
+add_action( 'wp_enqueue_scripts', 'boldgrid_enqueue_styles' );
 
